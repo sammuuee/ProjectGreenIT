cc7d296c747898327473a889753b6ca09967ff91
Samuel <sam.dasilva64@gmail.com>
2025-04-26 11:21:42 +0200

    changement pour en ligne

--- timelink/timelink-server/database/db.js
+++ timelink/timelink-server/database/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root', // remplace par ton mot de passe MySQL si besoin
-  database: 'timelink'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect(err => {
