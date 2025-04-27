8682b473aed608f521ad3235c80ce979d5f023e0
Samuel <sam.dasilva64@gmail.com>
2025-04-27 14:15:13 +0200

    fix

--- timelink/timelink-server/index.js
+++ timelink/timelink-server/index.js
@@ -4,9 +4,9 @@ const userRoutes = require('./routes/users');
 const messageRoutes = require('./routes/messages');
 const duelRoutes = require('./routes/duels');
 const friendsRoutes = require('./routes/friends');
-
+const cron = require('node-cron');
 const app = express();
-
+const db = require('./database/db');
 // CORS configuration
 app.use(cors({
   origin: 'https://project-green-it.vercel.app',
@@ -20,7 +20,22 @@ app.use('/api/users', userRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/duels', duelRoutes);
 app.use('/api/friends', friendsRoutes);
+cron.schedule('0 * * * *', () => {
+  console.log('⏰ Nettoyage des messages anciens...');
 
+  const query = `
+    DELETE FROM messages
+    WHERE created_at < NOW() - INTERVAL 24 HOUR
+  `;
+
+  db.query(query, (err, result) => {
+    if (err) {
+      console.error('Erreur lors du nettoyage des messages :', err);
+    } else {
+      console.log(`✅ Messages supprimés : ${result.affectedRows}`);
+    }
+  });
+});
 // server
 const PORT = process.env.PORT || 3000;
 
